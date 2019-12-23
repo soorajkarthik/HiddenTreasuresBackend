9004ace332c5c3aef5564eb5cae11762fa450789
Sooraj Karthik <soorajkarthik15@gmail.com>
2019-12-23 10:21:30 -0600

    initial commit

--- functions/index.js
+++ functions/index.js
@@ -7,5 +7,5 @@ admin.initializeApp();
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+//  response.send("Hello from Firebase  !");
 // });
